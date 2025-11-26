466fb82e43f6564fa0c21323bd7142ee5edbda29
areumj <areumj0507@gmail.com>
2025-11-26 21:05:53 +0900

    feat: (문자열) 신규 아이디 추천 풀이 추가

---
+++ src/string_a/NewIdRecommendation.java
@@ -0,0 +1,60 @@
+package string_a;
+
+public class NewIdRecommendation {
+    public static String solution(String new_id) {
+        // 1단계: 모든 대문자를 소문자로 치환
+        new_id = new_id.toLowerCase();
+
+        // 2단계: 알파벳 소문자, 숫자, -, _, .를 제외한 모든 문자를 제거
+        new_id = new_id.replaceAll("[^a-z0-9_.-]", "");
+
+        // 3단계: 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환
+        new_id = new_id.replaceAll("\\.{2,}", ".");
+
+        // 4단계: 마침표(.)가 처음이나 끝에 위치한다면 제거
+        if(new_id.startsWith(".")) {
+            new_id = new_id.substring(1);
+        }
+
+        if(new_id.endsWith(".")) {
+            new_id = new_id.substring(0, new_id.length() - 1);
+        }
+
+        // 5단계: 빈 문자열이라면, new_id에 "a"
+        if(new_id.isBlank()) {
+            new_id = "a";
+        }
+
+        // 6단계: 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거
+        // 6-1단계: 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거
+        if(new_id.length() > 15) {
+            new_id = new_id.substring(0, 15);
+
+            if(new_id.endsWith(".")) {
+                new_id = new_id.substring(0, new_id.length() - 1);
+            }
+        }
+
+        // 7단계: new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙임
+        if(new_id.length() < 3) {
+            StringBuilder newIdBuilder = new StringBuilder(new_id);
+            String lastWord = new_id.substring(new_id.length() - 1);
+            int loopCount = 3 - (new_id.length());
+            newIdBuilder.append(lastWord.repeat(loopCount));
+            new_id = newIdBuilder.toString();
+        }
+
+        return new_id;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("...!@BaT#*..y.abcdefghijklm"));    // "bat.y.abcdefghi"
+        System.out.println(solution("z-+.^."));                         // "z--"
+        System.out.println(solution("=.="));                            // "aaa"
+        System.out.println(solution("123_.def"));                       // "123_.def"
+        System.out.println(solution("abcdefghijklmn.p"));               // "abcdefghijklmn"
+        System.out.println(solution("HELLOWORLDCOMPUTERCODINGTEST123..."));
+        System.out.println(solution(""));
+        System.out.println(solution("AA"));
+    }
+}
